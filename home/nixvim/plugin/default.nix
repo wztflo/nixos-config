707d1556d86d4a007a87c2795e3a5a909344caaf
wztflo <210134871+wztflo@users.noreply.github.com>
2026-05-20 17:26:42 +0800

    feat(nixvim): add treesitter

--- home/nixvim/plugin/default.nix
+++ home/nixvim/plugin/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./ui  # 引入 UI 类插件（会自动找 ui/default.nix，内部可再引入 lualine 等）
     ./lsp # 引入 LSP 类插件（会自动找 lsp/default.nix，内部引入 nvim-cmp 等）
+    ./editor
   ];
 }
---
+++ home/nixvim/plugin/editor/treesitter.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }: {
+  programs.nixvim = {
+    plugins = {
+      treesitter = {
+        enable = true;
+        folding.enable = false;
+        nixvimInjections = true;
+        
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+          incremental_selection.enable = true;
+        };
+
+        grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+          bash
+          lua
+          python
+          rust
+          go
+          javascript
+          typescript
+          tsx
+          html
+          css
+          json
+          toml
+          nix
+          markdown
+          vim
+          vimdoc
+          xml
+          yaml
+          diff
+        ];
+      };
+
+      treesitter.lazyLoad = {
+        enable = true;
+        settings = {
+          event = [ "BufReadPost" "BufNewFile" ];
+        };
+      };
+    };
+  };
+}
