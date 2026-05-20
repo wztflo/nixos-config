00b6c478f3ca84de902c490ec412d618a08b5695
wztflo <210134871+wztflo@users.noreply.github.com>
2026-05-20 11:36:43 +0800

    feat: add VS Code Remote WSL support

--- hosts/WSL/default.nix
+++ hosts/WSL/default.nix
@@ -9,6 +9,13 @@
 
   wsl.enable = true;
   wsl.defaultUser = "wztflo";
+  wsl.interop.register = true;
+
+  #vscode-remote 需要
+  environment.systemPackages = [
+    pkgs.wget
+  ];
+  programs.nix-ld.enable = true;
 
   system.stateVersion = "26.05";
 }
